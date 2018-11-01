f3358ff3a536a9212a33ab175744d4ee114646e1
Kirk.Wang <kirk.w.wang@gmail.com>
2018-11-01 13:56:12 +0800

    udpate SteelLayer & BrickLayer

--- game/components/GameFieldContent.tsx
+++ game/components/GameFieldContent.tsx
@@ -2,16 +2,18 @@ import _ from "lodash";
 import React from "react";
 import { State } from "../reducers";
 import { BLOCK_SIZE as B } from "../utils/constants";
+import { BrickLayer } from "./BrickLayer";
 import { SteelLayer } from "./SteelLayer";
 
 export class GameFieldContent extends React.PureComponent<Partial<State & Point>> {
     public render() {
         const { x = 0, y = 0, map } = this.props;
-        const { steels } = map.toObject();
+        const { steels, bricks } = map.toObject();
         return (
             <g className="battle-field" transform={`translate(${x},${y})`}>
                 <rect width={13 * B} height={13 * B} fill="#000000" />
                 <SteelLayer steels={steels} />
+                <BrickLayer bricks={bricks} />
             </g>
         );
     }
